2cf5d53336e368073abaceef67174b5b7c9a79f1
Toan Pham <phamtony1911@gmail.com>
2023-02-22 10:41:04 +0000

    Change port for backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
