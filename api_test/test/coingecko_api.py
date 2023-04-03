3357cde0d0393c9e039464693d3acd79bbdc6142
jeppuka <jesper.k.kahari@gmail.com>
2023-04-04 07:42:15 +0800

    delete unwanted files

--- api_test/test/coingecko_api.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, json 
-
-url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=gbp&days=1"
-
-with urllib.request.urlopen(url) as url:
-    data = json.load(url)
-
-print(data)
