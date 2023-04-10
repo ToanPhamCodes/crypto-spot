5ec0841313561a8e3076a268a212145508b04b5e
jeppuka <jesper.k.kahari@gmail.com>
2023-04-10 11:42:39 +0100

    fixed search results not responding to delete

--- frontend/src/components/TickerSearch/index.js
+++ frontend/src/components/TickerSearch/index.js
@@ -7,17 +7,22 @@ const TickerSearch = () => {
   const [days, setDays] = useState(5);
   const [tokenData, setTokenData] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
+  const [originalSearchResults, setOriginalSearchResults] = useState(null);
 
   const handleTestButton = (token) => {
     setTicker(token);
   };
 
   const handleSearchInputChange = (event) => {
-    const userInput = document.getElementById('dashBoardSearchInput').value.toLowerCase();
-    const filteredResults = searchResults.filter((result) =>
-      result.name.toLowerCase().includes(userInput)
-    ).slice(0, 4);
-    setSearchResults(filteredResults);
+    const userInput = event.target.value.toLowerCase();
+    if (userInput === '') {
+      setSearchResults(originalSearchResults);
+    } else {
+      const filteredResults = originalSearchResults.filter((result) =>
+        result.name.toLowerCase().includes(userInput)
+      );
+      setSearchResults(filteredResults);
+    }
   };
 
   useEffect(() => {
@@ -34,37 +39,20 @@ const TickerSearch = () => {
       const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=30&page=1&sparkline=false&price_change_percentage=24h`);
       const data = await response.json();
       setSearchResults(data);
+      setOriginalSearchResults(data);
     };
     fetchSearchResults();
   }, []);
 
   return (
     <div className='App'>
-<<<<<<< HEAD
       <div className='GraphChart'>
         <PriceChart ticker={ticker} days={days} />
         <div className='intervals'>
-=======
-      <input
-        type="text"
-        placeholder="Enter a ticker symbol (e.g. bitcoin)"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-
-      <button onClick={handleSearch}>Search</button>
-
-      <div className='GraphComponent'> 
-        <div className='GraphChart'>
-          
-          <PriceChart ticker={ticker} days={days} setDays={setDays} />
-          <button onClick={() => setDays(1)}>1 Day</button>
-          <button onClick={() => setDays(5)}>5 Days</button>
-          <button onClick={() => setDays(10)}>10 Days</button>
-          <button onClick={() => setDays(15)}>15 Days</button>
-          <button onClick={() => setDays(20)}>20 Days</button>
-          <button onClick={() => setDays(25)}>25 Days</button>
->>>>>>> 99a936f4cba99f5cde4c2865608da7ac308e36f6
+        <button onClick={() => setDays(1)}>1 Day</button>
+        <button onClick={() => setDays(3)}>3 Days</button>
+        <button onClick={() => setDays(7)}>7 Days</button>
+        <button onClick={() => setDays(14)}>14 Days</button>
           <button onClick={() => setDays(30)}>30 Days</button>
         </div>
       </div>
@@ -79,17 +67,17 @@ const TickerSearch = () => {
         )}
       </div>
       <div className='dashBoardSearchDiv'>
-  <input id='dashBoardSearchInput'className='dashBoardSearchInput' onChange={handleSearchInputChange} />
-  <div className='dashBoardSearchResultsDiv'>
-    {searchResults.map((result) => (
-      <div className='testToken' key={result.id} onClick={() => handleTestButton(result.id)}>
-        <img src={result.image} alt={`${result.name} logo`} height='48px' />
-        <p>{result.name}</p>
-        <p>{result.current_price} GBP</p>
+        <input className='dashBoardSearchInput' onChange={handleSearchInputChange} />
+        <div className='dashBoardSearchResultsDiv'>
+          {searchResults.map((result) => (
+            <div className='testToken' key={result.id} onClick={() => handleTestButton(result.id)}>
+              <img src={result.image} alt={`${result.name} logo`} height='48px' />
+              <p>{result.name}</p>
+              <p>{result.current_price} GBP</p>
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-  </div>
-</div>
     </div>
   );
 };
