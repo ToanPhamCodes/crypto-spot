697bf06ec0a72cb28932f6c8f48d0a9478e44198
Abubakar Aqiil <74040264+Aqiil@users.noreply.github.com>
2023-04-11 17:49:05 +0100

    Add endpoint to retrieve all users

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -4,3 +4,18 @@ from db import db
 from models.user import User
 
 app = APIRouter()
+
+#Get a list of all users
+@app.get("/users", response_model=list[User])
+async def get_users():
+    users = db.users.find({})
+    return list(users)
+
+#Get a specific user by id
+
+#Create a new user
+
+#Update a user
+
+#Delete a user
+
